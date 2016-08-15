335dbf079c95245224fbd36029ff05a18da60231
vicanio <67449593@qq.com>
2016-08-15 16:27:32 +0800

    d

--- mem.py
+++ mem.py
@@ -1,4 +1,23 @@
 def meb(x,a,b,c):
 	if x < a :
 		return 0
-	if x > a and x <= b
+	if x > a and x <= b:
+		return (x-a)/(b-a)
+	if (x > b) and (x <= c):
+		return (c-x)/(c-b)
+	if x > c:
+		return 0
+	if a == b :
+		if x <= b:
+			return 1
+		if (x > b) and (x <= c):
+			return (c-x)/(c-b)
+		if x > c:
+			return 0
+	if b == c:
+		if x <= a:
+			return 0
+    if (x > a) and (x <= b):
+        return (x-a)/(b-a)
+    if x > b:
+        return 1
